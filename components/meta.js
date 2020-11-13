96aab4249072b8746264616b7d29ac5ba276965c
Edouard Cockenpot <edouard@cockenpot.net>
2020-11-13 18:22:06 +0100

    update meta

--- components/meta.js
+++ components/meta.js
@@ -1,35 +1,43 @@
 import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+import {HOME_OG_DESCRIPTION, HOME_OG_IMAGE_URL, HOME_OG_TITLE} from '../lib/constants'
 
 export default function Meta() {
-  return (
-    <Head>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="48x48"
-        href="/favicon/favicon.png"
-      />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
-      <link rel="shortcut icon" href="/favicon/icon.png" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
-      <meta
-        name="description"
-        content={`Petite entreprise familiale de restauration rapide, traiteur et rotisserie. Notre savoir faire et notre proximité avec nos clients nous permet de facilement comprendre leurs besoins et d’adapter nos propositions au plus proche de leurs attentes. Notre chef, au quotidien, axe son travail sur la créativité et l’originalité.`}
-      />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
-    </Head>
-  )
+    return (
+        <Head>
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/favicon/apple-touch-icon.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="48x48"
+                href="/favicon/favicon.png"
+            />
+            <link rel="manifest" href="/favicon/site.webmanifest"/>
+            <link
+                rel="mask-icon"
+                href="/favicon/safari-pinned-tab.svg"
+                color="#000000"
+            />
+            <link rel="shortcut icon" href="/favicon/icon.png"/>
+            <meta name="msapplication-TileColor" content="#000000"/>
+            <meta name="msapplication-config" content="/favicon/browserconfig.xml"/>
+            <meta name="theme-color" content="#000"/>
+            <meta
+                name="description"
+                content={HOME_OG_DESCRIPTION}
+            />
+            <meta property="og:image" content={HOME_OG_IMAGE_URL}/>
+            <meta property="og:title" content={HOME_OG_TITLE}/>
+            <meta property="og:image:height" content="400"/>
+            <meta property="og:image:width" content="700"/>
+            <meta property="og:description" content={HOME_OG_DESCRIPTION}/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:site_name" content={HOME_OG_TITLE}/>
+            <meta property="og:locale" content="fr_FR"/>
+
+        </Head>
+    )
 }
