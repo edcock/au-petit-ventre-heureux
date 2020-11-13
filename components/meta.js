4f991f5fba613834df6f7b5e6dd47f72a7a841b7
Edouard Cockenpot <edouard@cockenpot.net>
2020-11-13 18:24:59 +0100

    update meta

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import {HOME_OG_DESCRIPTION, HOME_OG_IMAGE_URL, HOME_OG_TITLE} from '../lib/constants'
+import {HOME_OG_DESCRIPTION, HOME_OG_IMAGE_URL, HOME_OG_TITLE, HOME_OG_URL} from '../lib/constants'
 
 export default function Meta() {
     return (
@@ -37,6 +37,7 @@ export default function Meta() {
             <meta property="og:type" content="website"/>
             <meta property="og:site_name" content={HOME_OG_TITLE}/>
             <meta property="og:locale" content="fr_FR"/>
+            <meta property="og:url" content={HOME_OG_URL} />
 
         </Head>
     )
