d48ea1b12eb45c9f597729e9b9e84d063ae8b6c1
Edouard Cockenpot <edouard@cockenpot.net>
2021-02-08 08:57:48 +0100

    udpate signature

--- components/layout.js
+++ components/layout.js
@@ -11,7 +11,10 @@ export default function Layout({preview, children}) {
                 <Header/>
                 <main>{children}</main>
                 <Footer/>
-                <Link href={'/mention-legales'}><a className='link dark-gray tc pb3'>Mentions légales</a></Link>
+                <div className="flex flex-row-ns flex-column justify-center">
+                    <Link href={'/mention-legales'}><a className='link dark-gray tc pb3 ph3'>Mentions légales</a></Link>
+                    <Link href={'https://mister-e.co'} ><a className='link dark-gray tc pb3 ph3' target={'_blank'}>Site internet réalisé par Mister-E</a></Link>
+                </div>
             </div>
         </>
     )
