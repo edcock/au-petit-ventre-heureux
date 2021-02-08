fcda91611b6df2863c01c21f58e31f996aa330ea
Edouard Cockenpot <edouard@cockenpot.net>
2021-02-08 06:39:38 +0100

    update valentine day menu

--- pages/selection-du-jour.js
+++ pages/selection-du-jour.js
@@ -6,6 +6,8 @@ import {renderPriceWithSpaces} from "../lib/helper-price";
 export async function getServerSideProps() {
     const SELECTION_DU_JOUR_QUERY = `{ allDailyDishes(filter: {active: {eq: true}}) { id
     price
+    appetizer
+    drink
     name
     mainCourse
     starter
@@ -35,15 +37,19 @@ export default function SelectionDuJour({data}) {
     return (
         <Layout>
             <div className="dishes-container">
-                <div className="flex flex-row-ns flex-column flex-wrap vh-75-ns items-center">
+                <div className="flex flex-row-ns flex-column flex-wrap items-start mv5">
                     {data?.allDailyDishes.map((dailyDish, index) => {
                         return (
                             <div key={dailyDish.id}
                                  className="daily-container flex flex-column items-center">
-                                <div className="w-100 pr3-ns w-50-ns order-2 h-100 flex flex-column justify-around">
+                                <div className="w-100 w-50-ns order-2 h-100 flex flex-column justify-around mt4">
                                     <h1 className="daily-dish-title">
                                         {dailyDish.name} - {renderPriceWithSpaces(dailyDish.price)}
                                     </h1>
+                                    {dailyDish.appetizer && (<div className="daily-starter">
+                                        <p>{dailyDish.appetizer}</p>
+                                    </div>)
+                                    }
                                     <div className="daily-starter">
                                         <p>{dailyDish.starter}</p>
                                         {dailyDish.secondStarter
@@ -70,6 +76,9 @@ export default function SelectionDuJour({data}) {
                                         </>
                                         }
                                     </div>
+                                    {dailyDish.drink && (<div className="daily-drink">
+                                        <p>{dailyDish.drink}</p>
+                                    </div>)}
                                 </div>
                                 <div className="mb3 mb0-ns w-100 w-50-ns order-1 pa6 cover br3"
                                      style={{background: `url(${dailyDish.picture.url})`}}>
